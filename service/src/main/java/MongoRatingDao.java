0370ac75c562fd2a4046cda243dcdd3ec0c28e36
Karan G <karangb@gmail.com>
2013-09-06 22:07:44 +0100

    Added missing file in git

---
+++ service/src/main/java/MongoRatingDao.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: @karangb
+ * Date: 06/09/2013
+ * Time: 22:06
+ */
+public class MongoRatingDao implements RatingDAO {
+    @Override
+    public Rating getRating(String username) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}

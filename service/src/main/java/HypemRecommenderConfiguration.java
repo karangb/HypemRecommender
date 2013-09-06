c1f2986eb66d31bc93017ba5a5cf9f1fd71b6bb3
Karan G <karangb@gmail.com>
2013-09-06 20:18:00 +0100

    Added HypemRecommender's Service and Configuration files (main files)

---
+++ service/src/main/java/HypemRecommenderConfiguration.java
@@ -0,0 +1,12 @@
+import com.yammer.dropwizard.config.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: @karangb
+ * Date: 04/09/2013
+ * Time: 22:51
+ */
+public class HypemRecommenderConfiguration extends Configuration {
+
+
+}
---
+++ service/src/main/java/HypemRecommenderService.java
@@ -0,0 +1,26 @@
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: @karangb
+ * Date: 04/09/2013
+ * Time: 22:51
+ */
+public class HypemRecommenderService extends Service<HypemRecommenderConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HypemRecommenderService().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<HypemRecommenderConfiguration> bootstrap) {
+        bootstrap.setName("hypem-recommender");
+    }
+
+    @Override
+    public void run(HypemRecommenderConfiguration hypemRecommenderConfiguration, Environment environment) throws Exception {
+        environment.addResource(new RatingResource(new MongoRatingDao()));
+    }
+}

5c261883c57741d417d7ee275fabe8e487095da2
Karan G <karangb@gmail.com>
2013-11-12 21:46:43 +0000

    using FakeRepresentation in MyrrixTrackDaoTests

--- dal/src/test/java/com/hypemrecommender/dal/MyrrixTrackDaoTests.java
+++ dal/src/test/java/com/hypemrecommender/dal/MyrrixTrackDaoTests.java
@@ -32,18 +32,11 @@ public class MyrrixTrackDaoTests extends MongoFixture{
     @Test
     public void testProvision()
     {
-        HypemTrackRepresentation track = new HypemTrackRepresentation("1a13f", "Parov Stelar", "The Golden Boy", 30, "http://static-ak.hypem.net/thumbs_new/25/1434405.jpg", "http://static-ak.hypem.net/thumbs_new/25/1434405_120.jpg", "http://static-ak.hypem.net/thumbs_new/25/1434405_320.jpg");
+        HypemTrackRepresentation track = FakeRepresentations.track1();
         trackDao.provision(track);
 
-        assertTrackProvisioned(track);
-    }
-
-    private void assertTrackProvisioned(final HypemTrackRepresentation track) {
         DBObject trackDoc = trackCollection.findOne(new BasicDBObject("media_id", track.getMediaId()));
-        assertThat((String)trackDoc.get("artist"), equalTo(track.getArtist()));
-        assertThat((String)trackDoc.get("title"), equalTo(track.getTitle()));
-        assertThat((String)trackDoc.get("thumb_url"), equalTo(track.getThumbUrl()));
-        assertThat((String)trackDoc.get("thumb_url_medium"), equalTo(track.getThumbUrlMedium()));
-        assertThat((String)trackDoc.get("thumb_url_large"), equalTo(track.getThumbUrlLarge()));
+        trackDoc.removeField("_id");
+        assertThat(trackDoc, equalTo(FakeRepresentations.track1Doc()));
     }
 }

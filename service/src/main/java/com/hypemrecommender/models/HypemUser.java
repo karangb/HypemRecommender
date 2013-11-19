aacde8ef808faaa34b98c43f95171b335a89085b
Karan G <karangb@gmail.com>
2013-11-19 23:02:10 +0000

    added top level implementation of crawler

--- service/src/main/java/com/hypemrecommender/models/HypemUser.java
+++ service/src/main/java/com/hypemrecommender/models/HypemUser.java
@@ -4,6 +4,8 @@ import com.hypemrecommender.Crawler;
 import com.hypemrecommender.dal.UserDao;
 import com.hypemrecommender.representations.HypemUserRepresentation;
 
+import java.util.Collection;
+
 /**
  * Created with IntelliJ IDEA.
  * User: @karangb
@@ -34,5 +36,20 @@ public class HypemUser implements User{
             HypemUserRepresentation userRepresentation = crawler.fetchUser(username);
             userDao.provision(userRepresentation);
         }
+    }
+
+    @Override
+    public Void provision() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Collection<Track> getFavourites() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean exists() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
---
+++ service/src/main/java/com/hypemrecommender/models/Track.java
@@ -0,0 +1,13 @@
+package com.hypemrecommender.models;
+
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: @karangb
+ * Date: 19/11/2013
+ * Time: 18:53
+ */
+public interface Track {
+    Collection<User> getFavouritedBy();
+}

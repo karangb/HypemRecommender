2f2bcb8f3fdd72a6f17dc8ccdc45edd9b4faa2db
Karan G <karangb@gmail.com>
2013-11-30 22:42:33 +0000

    added extra test - checking if new track is provisioned properly

--- dal/src/test/java/com/hypemrecommender/dal/SoundcloudUserDaoTest.java
+++ dal/src/test/java/com/hypemrecommender/dal/SoundcloudUserDaoTest.java
@@ -23,16 +23,15 @@ import static org.mockito.Mockito.when;
  * Time: 14:19
  */
 public class SoundcloudUserDaoTest extends MongoFixture{
-    @Mock CloudTrack track;
     private DBCollection userCollection;
     private DBCollection trackCollection;
     private ObjectId existingTrackId;
     private int soundcloudUserId;
+    private SoundcloudUserDao userDao;
 
     @Before
     public void setUp()
     {
-        MockitoAnnotations.initMocks(this);
         userCollection = testDb.createCollection("users", new BasicDBObject());
         trackCollection = testDb.createCollection("tracks", new BasicDBObject());
 
@@ -43,22 +42,30 @@ public class SoundcloudUserDaoTest extends MongoFixture{
         BasicDBObject existingTrackDoc = new BasicDBObject("soundcloudId", "1234");
         trackCollection.insert(existingTrackDoc);
         existingTrackId = (ObjectId) existingTrackDoc.get("_id");
-
-        when(track.getId()).thenReturn("1234");
-        when(track.getArtist()).thenReturn("myArtist");
-        when(track.getTitle()).thenReturn("myTitle");
-        when(track.getStreamUrl()).thenReturn("http://someUrl");
+        userDao = new SoundcloudUserDao(userCollection, trackCollection, soundcloudUserId);
     }
 
     @Test
     public void testProvisionFavouriteLinksUserFavourites()
     {
-        SoundcloudUserDao userDao = new SoundcloudUserDao(userCollection, trackCollection, soundcloudUserId);
-        userDao.provisionFavourite(track);
+        CloudTrack existingTrack = new FakeCloudTrack("1234", "myArtist", "myTitle", "http://someUrl");
+        userDao.provisionFavourite(existingTrack);
 
         DBObject myUser = userCollection.findOne(new BasicDBObject("soundcloudId", soundcloudUserId));
         ArrayList<ObjectId> favourites = (ArrayList<ObjectId>) myUser.get("favourites");
 
         assertThat(favourites.get(0), equalTo(existingTrackId));
+    }
+
+    @Test
+    public void testProvisionFavouriteProvisionsNewTrack()
+    {
+        final CloudTrack newTrack = new FakeCloudTrack("456", "myNewArtist", "myNewTitle", "http://someUrl");
+        userDao.provisionFavourite(newTrack);
+        DBObject track = trackCollection.findOne(new BasicDBObject("soundcloudId", "456"));
+
+        assertThat((String) track.get("artist"), equalTo("myNewArtist"));
+        assertThat((String) track.get("title"), equalTo("myNewTitle"));
+        assertThat((String) track.get("streamUrl"), equalTo("http://someUrl"));
     }
 }

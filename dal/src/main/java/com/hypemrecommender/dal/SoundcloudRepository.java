b8bebf95a4ecc7bff76a89207e75a353827d55c8
Karan G <karangb@gmail.com>
2013-12-01 13:40:36 +0000

    implemented userExists in SoundcloudRepository

--- dal/src/main/java/com/hypemrecommender/dal/SoundcloudRepository.java
+++ dal/src/main/java/com/hypemrecommender/dal/SoundcloudRepository.java
@@ -1,5 +1,8 @@
 package com.hypemrecommender.dal;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+
 /**
  * Created with IntelliJ IDEA.
  * User: @karangb
@@ -7,8 +10,22 @@ package com.hypemrecommender.dal;
  * Time: 22:07
  */
 public class SoundcloudRepository implements UserDaoRepository{
+    private final DBCollection userCollection;
+    private final DBCollection trackCollection;
+
+    public SoundcloudRepository(final DBCollection userCollection, final DBCollection trackCollection) {
+
+        this.userCollection = userCollection;
+        this.trackCollection = trackCollection;
+    }
+
     @Override
     public UserDao create(final String userId) {
-        return new SoundcloudUserDao(null, null, Integer.valueOf(userId));
+        return new SoundcloudUserDao(userCollection, trackCollection, Integer.valueOf(userId));
+    }
+
+    @Override
+    public boolean userExists(final String userId) {
+        return userCollection.find(new BasicDBObject("soundcloudId", userId)).limit(1).count() != 0;
     }
 }
